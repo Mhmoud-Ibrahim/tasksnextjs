c7f482e28ed96d1e6a36b510b0b93af23056cdfc
Mhmoud-Ibrahim <mahmoudd188@gmail.com>
2026-05-01 16:28:28 +0400

    fd

--- src/lib/api.tsx
+++ src/lib/api.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL:"https://noor-server-ts.vercel.app",
+    baseURL:"https://taskts.vercel.app",
     withCredentials: true 
 });
 
